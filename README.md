fef05057c0612e00f70c113b4de879878d918493
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-08-30 22:51:17 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Bootcamp - Homework 4: Code Quiz
+A short quiz on JavaScript
+
+## Purpose
+Students learning JavaScript
+
+## Main JavaScript Concepts Used
+
+
+This homework assignment is part of the [Carleton University Coding Bootcamp](https://bootcamp.carleton.ca/).
---
+++ script.js
@@ -0,0 +1,25 @@
+//when you press the start button
+  //timer starts
+  //display the question
+  //display the options
+
+//when user selects options
+  //go to next question
+  //if selection was wrong, deduct time, display "wrong" text
+  //if selection was correct, display "correct" text
+
+//if user makes it to end of questions with time left
+//or user runs out of time
+  //allow user to enter initials and save their remaining time as score
+  //save this to local storage
+  //redirect to highscores page
+
+//highscores page
+  //separate js file?
+  //grab the highscores from local storage
+  //if there's something returned, display it
+
+//if you clear highscores, clear it from local storage
+  //reload the content
+
+
