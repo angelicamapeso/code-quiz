ee4ff1191054e9323fd1c0bb8b4307fa0c0af08b
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-08-31 16:25:52 -0400

    Add noramlize, move script files into own folder

---
+++ scripts/highscore.js
@@ -0,0 +1,7 @@
+//highscores page
+  //separate js file?
+  //grab the highscores from local storage
+  //if there's something returned, display it
+
+//if you clear highscores, clear it from local storage
+  //reload the content
--- scripts/index.js
+++ scripts/index.js
@@ -14,12 +14,5 @@
   //save this to local storage
   //redirect to highscores page
 
-//highscores page
-  //separate js file?
-  //grab the highscores from local storage
-  //if there's something returned, display it
-
-//if you clear highscores, clear it from local storage
-  //reload the content
 
 
