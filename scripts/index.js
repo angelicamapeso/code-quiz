b2ceb4a886dc3ccc26270267fe6d16de38b8c195
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-09-01 13:33:12 -0400

    Add test question and event listener for start button

--- scripts/index.js
+++ scripts/index.js
@@ -5,15 +5,9 @@ const START_BTN = document.getElementById("start-button");
 //Quiz questions
 const QUIZ_SECTION = document.getElementById("quiz-questions");
 const TIME_REMAINING = document.getElementById("time-remaining");
+const QUESTION = document.getElementById("question");
 const CHOICES = document.getElementById("choices");
 const CHOICE_STATUS = document.getElementById("choice-status");
-
-//question object
-  //question
-  //array of answers
-  //index of correct answer
-
-//create array of questions
 
 //End
 const END_SECTION = document.getElementById("end");
@@ -21,7 +15,29 @@ const SCORE = document.getElementById("score");
 const INITIALS_INPUT = document.getElementById("initials");
 const SUBMIT_SCORE_BTN = document.getElementById("submit-score");
 
+class Question {
+  constructor(question, choices, indexOfCorrectChoice) {
+    this.question = question;
+    this.choices = choices;
+    this.indexOfCorrectChoice = indexOfCorrectChoice;
+  }
+}
+const QUESTION_1 = new Question("Something", ["option1", "option2", "option3"], 2);
+
 //when you press the start button
+START_BTN.addEventListener('click', function() {
+  START_SECTION.classList.add("hidden");
+  QUIZ_SECTION.classList.remove("hidden");
+
+  QUESTION.textContent = QUESTION_1.question;
+  QUESTION_1.choices.forEach(function(element, index) {
+    const li = document.createElement("li");
+    const button = document.createElement("button");
+    button.textContent = (index + 1) + ". " + element;
+    li.appendChild(button);
+    CHOICES.appendChild(li);
+  });
+});
   //timer starts
   //display the question
   //display the options
