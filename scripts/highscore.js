a0473da5b308bacd7bd64f53134c1aacb4ae21a7
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-09-02 17:07:18 -0400

    Add functions to display highscore and clear

--- scripts/highscore.js
+++ scripts/highscore.js
@@ -2,10 +2,37 @@
 const HIGHSCORE_TABLE = document.getElementById("highscores-table");
 const CLEAR_HIGHSCORE_BTN = document.getElementById("clear-highscores");
 
-//highscores page
-  //separate js file?
-  //grab the highscores from local storage
-  //if there's something returned, display it
+updateTable();
 
-//if you clear highscores, clear it from local storage
-  //reload the content
+function updateTable() {
+  let highscores = localStorage.getItem("scoreList");
+
+  if (highscores) {
+    highscores = JSON.parse(highscores);
+
+    highscores.forEach(function(scoreItem, index) {
+      const tableRow = document.createElement('tr');
+    
+      const rank = document.createElement('td');
+      rank.textContent = `#${index + 1}`;
+    
+      const score = document.createElement('td');
+      score.textContent = scoreItem.score;
+    
+      const initials = document.createElement('td');
+      initials.textContent = scoreItem.intials;
+    
+      tableRow.appendChild(rank);
+      tableRow.appendChild(score);
+      tableRow.appendChild(initials);
+    
+      HIGHSCORE_TABLE.appendChild(tableRow);
+    });
+  }
+}
+
+CLEAR_HIGHSCORE_BTN.addEventListener('click', function(){
+  let emptyHighscores = [];
+  localStorage.setItem('scoreList', emptyHighscores);
+  updateTable();
+});
